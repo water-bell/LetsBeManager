152460f61e330af92c896b62244aa63fb9dc407d
water-bell <wwkk548@gmail.com>
2022-08-03 21:17:15 +0900

    Main

---
+++ Let's be manager/Assets/Scripts/SJ/LoadBtn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadBtn : MonoBehaviour
+{
+    public string SceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadFunc()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
