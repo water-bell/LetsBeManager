14e19d5527b759aa182db31c7edea70945b9d8b1
water-bell <wwkk548@gmail.com>
2022-08-08 17:15:41 +0900

    Initial Data + Font fiexed

---
+++ Let's be manager/Assets/Scripts/SJ/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+                Destroy(this.gameObject);
+        }
+        
+    }
+    public int Gold = 0;
+    public int Popularity = 0;
+}
---
+++ Let's be manager/Assets/Scripts/SJ/TextData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextData : MonoBehaviour
+{
+    private DateTime InitTime = new DateTime(2022, 1, 1); // 초기 날짜 2022년 1월 1일
+    public Text DateText;
+    public Text PopularityText;
+    public Text GoldText;
+    private void Awake()
+    {
+        TextDate();
+        TextPopularity();
+        TextGold();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void TextDate()
+    {
+        DateText.text = InitTime.ToString("yyyy년 MM월 dd일");
+    }
+    private void TextPopularity()
+    {
+        PopularityText.text += GameManager.instance.Popularity.ToString();
+    }
+    private void TextGold()
+    {
+        GoldText.text = GameManager.instance.Gold.ToString() + "G";
+    }
+}
