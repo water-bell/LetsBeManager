f75ac8a50d5bf6d514c7d1ae4c627bf318caa7a2
water-bell <wwkk548@gmail.com>
2022-08-16 02:45:23 +0900

    Player Meeting Fixed
    최대, 최소 스탯 계산 기능 추가했습니다. 준혁님 플레이어 능력치 설정, 정민님 플레이어 경험치 설정 추가 후에 마무리 가능합니다.

--- Let's be manager/Assets/Scripts/SJ/Meeting.cs
+++ Let's be manager/Assets/Scripts/SJ/Meeting.cs
@@ -8,36 +8,57 @@ using UnityEngine;
 
 public class Meeting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    List<int> PlayerStat = new List<int>() { }; // 선수 능력치 가져와서 리스트에 저장
+
+    int MaxIndex;
+    int Max2ndIndex;
+    int MinIndex;
+    int Min2ndIndex;
+    int MeetingResult;
+    
     void Start()
     {
-        
+          
     }
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    void FindStat() // 스탯 최대 최소값 찾기
+    {  
+        MaxIndex = PlayerStat.IndexOf(PlayerStat.Max());
+        MinIndex = PlayerStat.IndexOf(PlayerStat.Min());
+        if(MeetingResult == 2)// 2번째로 낮은 스탯 찾기
+        {
+            PlayerStat.RemoveAt(MinIndex);
+            Min2ndIndex = PlayerStat.IndexOf(PlayerStat.Min());
+        }
+        else if(MeetingResult == 3)// 2번째로 높은 스탯 찾기
+        {
+            PlayerStat.RemoveAt(MaxIndex);
+            Max2ndIndex = PlayerStat.IndexOf(PlayerStat.Max());
+        }
+    }
     public void PlayerMeeting()
     {
         float[] probs = new float[] { 1f, 2f, 8f, 9f, 10f, 15f, 15f, 20f, 20f }; // 케이스별 확률
-        //float totalProbs = 100.0f;
         System.Random rand = new System.Random();
         float r = rand.Next(0, 101); // 랜덤 0~100
-        int MeetingResult = 0; //초기화
+
         float cumulative = 0.0f; // 가중치 초기값
 
         for (int i = 0; i < 9; i++) //케이스 수 만큼 가중치에 확률을 더해줌
         {
             cumulative += probs[i]; // 가중치
-
             if (r <= cumulative) // 가중치가 랜덤한 값을 넘어서면 그 케이스
             {
                 MeetingResult = i;
                 break;
             }
         }
-
+        FindStat(); // MeetingResult 값 받아와서 최대 최소 스탯 계산;
         switch (MeetingResult)
         {
             case 0:
@@ -73,8 +94,8 @@ public class Meeting : MonoBehaviour
                 //선수 가장 낮은 스탯 1가지 경험치 40 증가;
                 break;
             case 8:
-                Debug.Log("8");
-                break;
+                Debug.Log("8");               
+                break; //아무 일 없음
         }
     }
 }
