4f49ac543b6cdf5aadba78279d399e8cac92c9a3
groot_ljm <beckaljm@naver.com>
2022-08-16 18:00:10 +0900

    player training
    훈련창 일부 구현, 스탯관련 스크립트는 구체화 후에 작성할 예정

---
+++ Let's be manager/Assets/Scripts/JM/expoint.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Min
+{
+    public class expoint : MonoBehaviour
+    {
+        public int stat;
+        public int advant;
+
+    // Start is called before the first frame update
+    void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        private void stats()
+        {
+            if(advant <= 99)
+            {
+                advant += 1;
+            }
+            else
+            {
+                advant = 0;
+                stat += 1;
+            }
+            //자세하게 나오면 이후 수정예정
+        }
+    }
+
+}
+
