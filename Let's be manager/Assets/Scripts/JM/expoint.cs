8f525c510dddb959ec16e7a32998927cb9402ae4
groot_ljm <beckaljm@naver.com>
2022-08-20 12:40:44 +0900

    code fix
    각 훈련별 스탯,경험치 증가 메소도 구현(아직 스크립트 미완성입니다..)

--- Let's be manager/Assets/Scripts/JM/expoint.cs
+++ Let's be manager/Assets/Scripts/JM/expoint.cs
@@ -7,13 +7,77 @@ namespace Min
 {
     public class expoint : MonoBehaviour
     {
-        public int stat;
-        public int advant;
+        List<int> playerstat = new List<int>() { }; //선수 능력치
 
-    // Start is called before the first frame update
-    void Start()
+        public List<int> adv = new List<int>(); //선수 능력치의 경험치
+        
+
+        public void train1()
         {
+            playerstat[0] += 1;
+            if(playerstat[0]==100)
+            {
+                playerstat[0] = 0;
+                adv[0] += 1;
+            }
+            
+        }
 
+        public void train2()
+        {
+            playerstat[1] += 1;
+            if (playerstat[1] == 100)
+            {
+                playerstat[1] = 0;
+                adv[1] += 1;
+            }
+        }
+
+        public void train3()
+        {
+            playerstat[2] += 1;
+            if (playerstat[2] == 100)
+            {
+                playerstat[2] = 0;
+                adv[2] += 1;
+            }
+        }
+
+        public void train4()
+        {
+            playerstat[3] += 1;
+            if (playerstat[3] == 100)
+            {
+                playerstat[3] = 0;
+                adv[3] += 1;
+            }
+        }
+
+        public void train5()
+        {
+            playerstat[4] += 1;
+            if (playerstat[4] == 100)
+            {
+                playerstat[4] = 0;
+                adv[4] += 1;
+            }
+        }
+
+        public void train6()
+        {
+            playerstat[5] += 1;
+            if (playerstat[5] == 100)
+            {
+                playerstat[5] = 0;
+                adv[5] += 1;
+            }
+        }
+
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            
         }
 
         // Update is called once per frame
@@ -21,19 +85,7 @@ namespace Min
         {
 
         }
-        private void stats()
-        {
-            if(advant <= 99)
-            {
-                advant += 1;
-            }
-            else
-            {
-                advant = 0;
-                stat += 1;
-            }
-            //자세하게 나오면 이후 수정예정
-        }
+        
     }
 
 }
