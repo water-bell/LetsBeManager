b1c67d43e1c96b227b0e99dfe0a37bc1648a0fc9
penguiin1 <104720787+penguiin1@users.noreply.github.com>
2022-09-07 02:56:10 +0900

    Transfer UI + Reroll Function
    선수영입 UI 적용 + 리롤 기능 구현
    선수 정보 연동은 구현중

--- Let's be manager/Assets/Scripts/JH/_Script/SelectChar.cs
+++ Let's be manager/Assets/Scripts/JH/_Script/SelectChar.cs
@@ -5,13 +5,14 @@ using UnityEngine.UI;
 
 public class SelectChar : MonoBehaviour
 {
-    public Text statText;
-    public Text potenText;
-    public GameObject pitcher0;
-    public GameObject pitcher1;
-    public GameObject batter0;
-    public GameObject batter1;
-    
+    //활성화 시킬 오브젝트 변수들
+    [SerializeField] Text statText1;
+    [SerializeField] Text statText2;
+    [SerializeField] Text statText3;
+    [SerializeField] Text statText4;
+    [SerializeField] Text statText5;
+    [SerializeField] Text potenText;
+
     //게임에 존재하는 선수의 수
     static int p = 0; //투수
     static int b = 0; //타자
@@ -19,7 +20,12 @@ public class SelectChar : MonoBehaviour
     //임의로 저장할 변수들
     List<int> randList = new List<int>() { 0, 0, 0, 0, 0 };
     string randPoten;
-    
+
+    //투수 타자 정보창 이미지 변환 변수
+    [SerializeField] Sprite change_imgP;
+    [SerializeField] Sprite change_imgB;
+    Image thisImg;
+
 
     #region 투수 능력치 뽑기
     public void SelectPitcher()
@@ -73,6 +79,7 @@ public class SelectChar : MonoBehaviour
     }
     #endregion
 
+    /*
     public void PickButton()
     {
         int rannum = Random.Range(0, 2);
@@ -118,5 +125,47 @@ public class SelectChar : MonoBehaviour
             }
             else { rannum = 0; }
         }
+    }
+    */
+
+    public void RerolllButton()
+    {
+        int r = Random.Range(0, 2); //투수와 타자를 정할 랜덤변수
+
+
+        Character rPlayer = new Character();
+
+        List<int> s = rPlayer.RandStat();  //랜덤한 스탯 변수
+        string p = rPlayer.RandPoten(); //랜덤한 잠재력 변수
+
+        GameManager.instance.yesBtn = false;  //선수영입 상태를 false로 초기화
+        //리롤을 위해서 임의로 게임매니저에 저장
+        GameManager.instance.rPick = r;
+        GameManager.instance.rStat = s;
+        GameManager.instance.rPoten = p;
+
+        statText1.text = s[0].ToString();
+        statText2.text = s[1].ToString();
+        statText3.text = s[2].ToString();
+        statText4.text = s[3].ToString();
+        statText5.text = s[4].ToString();
+        potenText.text = p;
+
+
+        if (r == 0) //투수일때 UI를 투수 정보창으로 변경
+        {
+            thisImg = GameObject.Find("TransferPlayer").GetComponent<Image>();
+            thisImg.sprite = change_imgP;
+        }
+        if (r == 1) //타자일때 UI를 타자 정보창으로 변경
+        {
+            thisImg = GameObject.Find("TransferPlayer").GetComponent<Image>();
+            thisImg.sprite = change_imgB;
+        }
+    }
+
+    public void yesButton()
+    {
+        GameManager.instance.yesBtn = true;
     }
 }
--- Let's be manager/Assets/Scripts/SJ/GameManager.cs
+++ Let's be manager/Assets/Scripts/SJ/GameManager.cs
@@ -24,9 +24,20 @@ public class GameManager : MonoBehaviour
     public int Year = 0;
     public int Week = 1;
 
-    //선수정보 투수2,타자2
+    #region 선수정보 투수2,타자2
     public PlayerData pitcher0 = new PlayerData();
     public PlayerData pitcher1 = new PlayerData();
     public PlayerData batter0 = new PlayerData();
     public PlayerData batter1 = new PlayerData();
+
+    #endregion
+
+    #region 선수 영입시 필요한 변수
+    public bool yesBtn; //선수영입시 사용할 확인 값
+    public List<int> rStat = new List<int>() { 0, 0, 0, 0, 0 }; //랜덤스탯을 저장할 임의의 변수
+    public string rPoten = null; //랜덤 잠재력을 저장할 임의의 변수
+    public int rPick;
+
+    #endregion
+
 }
